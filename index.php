1a51e6bf0125df2a0db1995d8bdffc5dfece250b
Fish <fish-seo@test.com>
2022-11-17 13:59:44 +0300

    fish local 4

--- index.php
+++ index.php
@@ -2,13 +2,32 @@
 
 
 
-echo 'edit remote rest!!! проверка изменений';
 
 
-echo 'edit local rest!!! тест';
 
 
-echo 'edit remote rest!!! проверка изменений 777';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
