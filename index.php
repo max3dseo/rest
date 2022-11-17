7d569b7cfd13debb74033a0441d763b5bc3b8a10
Fish <fish-seo@test.com>
2022-11-17 16:10:30 +0300

    fish local 6

--- index.php
+++ index.php
@@ -18,8 +18,19 @@ class User
 	public function getname()
 	{
 		echo $this->name;
+		echo "<br>";
+		echo $this->age;
 	}
 }
+
+
+$mas = new User;
+
+$mas->name = "Fish";
+$mas->age = 18;
+
+$mas->getname();
+
 
 
 
